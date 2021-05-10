bee81dfa2ba8474ca91a14ffaaedf646e0dc7b39
EVE JOYCE <103681990@student.swin.edu.au>
2021-05-10 14:09:02 +1000

    Task 3 Complete

--- code.sql
+++ code.sql
@@ -115,3 +115,66 @@ CREATE TABLE BOOKING(
 
 select table_name from information_schema.tables;
 exec sp_columns project;
+
+/* 
+Task 3 - add data
+
+/* 
+TOUR(TourName, Description)
+PRIMARY KEY(TourName)
+*/
+
+INSERT INTO TOUR(TourName, Description) VALUES
+('North', 'Tour of wineries and outlets of the Bedigo and Castlemaine region'),
+('South', 'Tour of wineries and outlets of Mornington Penisula'),
+('West', 'Tour of wineries and outlets of the Geelong and Otways region');
+
+/* 
+CLIENT(ClientID, Surname, GivenName, Gender)
+PRIMARY KEY(ClientID)
+*/
+
+INSERT INTO CLIENT(ClientID, Surname, GivenName, Gender) VALUES
+('1', 'Price', 'Taylor', 'M'),
+('2', 'Gamble', 'Ellyse', 'F'),
+('3', 'Tan', 'Tilly', 'F');
+
+/* 
+EVENT(TourName, EventYear, EventMonth, EventDay, Fee)
+PRIMARY KEY(TourName, EventYear, EventMonth, EventDay)
+FOREIGN KEY(TourName) REFERENCES TOUR
+*/
+
+INSERT INTO EVENT(TourName, EventYear, EventMonth, EventDay, Fee) VALUES
+('North', 2016, 'Jan', 9,  200),
+('North', 2016, 'Feb', 13, 225),
+('South', 2016, 'Jan', 9, 200),
+('South', 2016, 'Jan', 16, 200),
+('West', 2016, 'Jan', 29, 225);
+
+/* 
+BOOKING(ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment)
+PRIMARY KEY(ClientID, TourName, EventYear, EventMonth, EventDay)
+FOREIGN KEY(ClientID) REFERENCES CLIENT
+FOREIGN Key(TourName, EventYear, EventMonth, EventDay) REFERENCES EVENT
+*/
+
+INSERT INTO BOOKING(ClientID, TourName, EventYear, EventMonth, EventDay, DateBooked, Payment) VALUES
+(2, 'North', 2016, 'Jan', 9, '16-December-2015', 200),
+(1, 'North', 2016, 'Jan', 9, '10-December-2015', 200),
+(1, 'North', 2016, 'Feb', 13, '08-January-2016', 225),
+(2, 'North', 2016, 'Feb', 13, '14-January-2016', 125),
+(3, 'North', 2016, 'Feb', 13, '03-February-2016', 225),
+(1, 'South', 2016, 'Jan', 9, '10-December-2015', 200),
+(2, 'South', 2016, 'Jan', 16, '18-December-2015', 200),
+(3, 'South', 2016, 'Jan', 16, '09-January-2016', 200),
+(2, 'West', 2016, 'Jan', 29, '17-December-2015', 225),
+(3, 'West', 2016, 'Jan', 29, '18-December-2015', 200);
+
+
+
+select * from TOUR;
+select * from CLIENT;
+select * from EVENT;
+select * from BOOKING;
+
